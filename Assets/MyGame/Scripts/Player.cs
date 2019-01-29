9d683e99e6e7c01981f14ed7c2e194f6a97672b0
Leonhard Schnaitl <leonhard.schnaitl@htl-salzburg.ac.at>
2019-01-29 10:17:16 +0100

    updated readme and started implementing

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float speed = 2;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//move the player left and right
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1 * speed, rb.velocity.y);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -4,10 +4,21 @@ Simple Unity game. Try to get as high as can.
 Dev-Platform: Win 10, Unity 2018.2.14f1, Visual Studio Community 15.9.3
 Target-Platfrom: Standalone PC
 
-Status: conception
+Status: implementation
 
 Controls: On X-Axis, with AD or Left and Right.
 
 Concept:
 
+NEW: The base mechanic stays the same but the new goal is to just stay alive. Platforms are rising from below with random speed values.
+They have deadly spikes on the lower side of them, if touched by the player, its game over.
+If platforms hit one another from below, the one coming from below, thus touching the spikes, breaks (disappears). 
+This forces the player, when standing on the breaking platform to drop to another one underneath him.
+The Player dies when:
+ + touching the spikes of any platform
+ + falling out of the cameras view
+ + hitting the top of the cameras view
+
+
+OLD:
 <img src="Documents/concept-liftup.jpeg" width="250">
